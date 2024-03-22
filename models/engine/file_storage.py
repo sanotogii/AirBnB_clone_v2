577f4cce1b0715fcc20c6f0e2b183d14d54d7e0c
TheOrbMaverick <enangita@gmail.com>
2024-03-22 23:10:37 +0100

    che

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,43 +4,37 @@ FileStorage module
 """
 import json
 
-
 class FileStorage:
-    """ FileStorage class
-    """
+    """ FileStorage class """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """ Return all objects
-        """
+        """ Return all objects or objects of a specific class """
         if cls is None:
             return self.__objects
         return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
 
     def new(self, obj):
-        """ Add new object
-        """
-        key = obj.__class__.__name__ + "." + obj.id
+        """ Add new object to storage """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def delete(self, obj=None):
-        """ Delete an object
-        """
+        """ Delete an object from storage """
         if obj is None:
             return
-        key = obj.__class__.__name__ + "." + obj.id
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects.pop(key, None)
 
     def save(self):
-        """ Save objects to file
-        """
+        """ Save objects to file """
         with open(self.__file_path, mode="w", encoding="utf-8") as file:
             json.dump({k: v.to_dict() for k, v in self.__objects.items()}, file)
 
     def reload(self):
-        """ Reload objects from file
-        """
+        """ Reload objects from file """
         try:
             with open(self.__file_path, mode="r", encoding="utf-8") as file:
                 objs = json.load(file)
