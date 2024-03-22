10069be1fca51b30e245bc087375871438981301
TheOrbMaverick <enangita@gmail.com>
2024-03-22 23:06:59 +0100

    che

--- models/base_model.py
+++ models/base_model.py
@@ -1,96 +1,50 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
-from datetime import datetime
+import datetime
 
+# Create a base class for SQLAlchemy models
+Base = declarative_base()
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """BaseModel class representing the base for all models"""
 
-    def __init__(self, **kwargs):
+    # Define columns for id, created_at, and updated_at
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow())
 
-        """
-        Initializes a new instance of the BaseModel class.
-
-        Attributes:
-            - id (str): Unique identifier for the instance.
-            - created_at (datetime): Date and time of instance creation.
-            - updated_at (datetime): Date and time of last update.
-        """
-        if not kwargs:
-            # Handle the case when no kwargs are provided
-            # For example, when creating a new instance without arguments
-            print("Creating an empty instance of BaseModel")
-        else:
-            # Extract required attributes from kwargs or set default values
-            self.id = kwargs.get('id', str(uuid.uuid4()))
-            self.created_at = kwargs.get('created_at', datetime.now())
-            self.updated_at = kwargs.get('updated_at', datetime.now())
-
-            # Handle arbitrary attributes using kwargs
-            for key, value in kwargs.items():
-                if key not in ['id', 'created_at', 'updated_at']:
-                    setattr(self, key, value)
-
-        # if kwargs:
-        #     # Reconstruct an instance from a dictionary representation
-        #     if "__class__" in kwargs:
-        #         del kwargs["__class__"]  # Remove __class__ from the dictionary
-        #     # convert to datetime objects
-        #     kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-        #                                              "%Y-%m-%dT%H:%M:%S.%f")
-        #     kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-        #                                              "%Y-%m-%dT%H:%M:%S.%f")
-
-        #     # Assign the values to instance attributes
-        #     for key, value in kwargs.items():
-        #         setattr(self, key, value)
-
-        # else:
-        #     from models import storage
-        #     self.id = str(uuid.uuid4())
-        #     self.created_at = datetime.now()
-        #     self.updated_at = datetime.now()
-        #     storage.new(self)
-        #     storage.save()
-
-    # def __init__(self, *args, **kwargs):
-
-    #     """Instatntiates a new model"""
-
-    #     if not kwargs:
-    #         from models import storage
-    #         self.id = str(uuid.uuid4())
-    #         self.created_at = datetime.now()
-    #         self.updated_at = datetime.now()
-    #         storage.new(self)
-    #     else:
-    #         kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-    #                                                  '%Y-%m-%dT%H:%M:%S.%f')
-    #         kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-    #                                                  '%Y-%m-%dT%H:%M:%S.%f')
-    #         del kwargs['__class__']
-    #         self.__dict__.update(kwargs)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance"""
+        # Generate a unique ID for the object
+        self.id = str(uuid.uuid4())
+        # Set the creation and update timestamps to the current time
+        self.created_at = self.updated_at = datetime.datetime.now()
+        # Assign any additional keyword arguments passed to the constructor as attributes
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Save the object to the database"""
+        # Import storage module to access storage methods
         from models import storage
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        # Add the object to storage and save changes
+        storage.new(self)
         storage.save()
 
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+    def delete(self):
+        """Delete the object from the database"""
+        # Import storage module to access storage methods
+        from models import storage
+        # Delete the object from storage
+        storage.delete(self)
     
+    def to_dict(self):
+        """Convert object attributes to dictionary"""
+        # Create a dictionary containing object attributes
+        data = dict(self.__dict__)
+        # Remove SQLAlchemy internal state attribute
+        data.pop('_sa_instance_state', None)
+        return data
