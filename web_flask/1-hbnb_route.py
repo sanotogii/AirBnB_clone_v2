622724f3593462fd90ddeedc0f58d5294f8d67c6
TheOrbMaverick <enangita@gmail.com>
2024-04-19 14:15:21 +0100

    who dey?

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Implementing a Flask app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    The home function decorated by / and unstrict slashes.
+
+    Returns:
+        str: A greeting message.
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def home():
+    """
+    The home function decorated by / and unstrict slashes.
+
+    Returns:
+        str: A greeting message.
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    """
+    For when the file is run directly.
+    """
+    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000")
