11278de5621ca677e96b4f87141d2625d0c90f9a
TheOrbMaverick <enangita@gmail.com>
2024-04-19 15:24:34 +0100

    who dey?

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,17 +9,17 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
-    """
-    The home function decorated by / and unstrict slashes.
+	"""
+	The home function decorated by / and unstrict slashes.
 
-    Returns:
-        str: A greeting message.
-    """
-    return "Hello HBNB!"
+	Returns:
+		str: A greeting message.
+	"""
+	return "Hello HBNB!"
 
 if __name__ == "__main__":
-    """
-    For when the file is run directly.
-    """
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port="5000")
+	"""
+	For when the file is run directly.
+	"""
+	app.run(debug=True)
+	app.run(host="0.0.0.0", port="5000")
