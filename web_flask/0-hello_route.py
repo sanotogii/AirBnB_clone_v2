c3089dda94c5cc7d0c36b896dbbc54a006a4ebc3
TheOrbMaverick <enangita@gmail.com>
2024-04-19 15:48:04 +0100

    who dey?

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,8 +15,7 @@ def home():
 	Returns:
 		str: A greeting message.
 	"""
-	return "Hello HBNB!"
+	return 'Hello HBNB!'
 
-if __name__ == "__main__":
-	app.run(debug=True)
+if __name__ == '__main__':
 	app.run(host='0.0.0.0', port='5000')
