071d9efda54c2a207d2ba78f25afaaa8f7a992d6
TheOrbMaverick <enangita@gmail.com>
2024-04-20 16:12:53 +0100

    regain

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,26 +3,6 @@
 FileStorage module
 """
 import json
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-import json
-
-classes = {
-    "BaseModel": BaseModel,
-    "User": User,
-    "Place": Place,
-    "State": State,
-    "City": City,
-    "Amenity": Amenity,
-    "Review": Review
-}
-"""classes: dictionary mapping class names to corresponding class objects"""
-
 
 class FileStorage:
     """ FileStorage class """
@@ -54,13 +34,11 @@ class FileStorage:
             json.dump({k: v.to_dict() for k, v in self.__objects.items()}, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
+        """ Reload objects from file """
         try:
-            with open(self.__file_path, 'r') as file:
-                deserialized_objects = json.load(file)
-                for key, obj_dict in deserialized_objects.items():
-                    class_name = obj_dict['__class__']
-                    self.__objects[key] = classes[class_name](**obj_dict)
+            with open(self.__file_path, mode="r", encoding="utf-8") as file:
+                objs = json.load(file)
+                self.__objects = {k: eval(v["__class__"])(**v) for k, v in objs.items()}
         except FileNotFoundError:
             pass
 
