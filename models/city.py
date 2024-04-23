9628b6dd14fe2760dbe1d71519e7196df5c97648
TheOrbMaverick <enangita@gmail.com>
2024-04-23 02:45:36 +0100

    the the wee hours

--- models/city.py
+++ models/city.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
 
-class City(BaseModel):
-    """ State class """
-    name = ""
-    __tablename__ = "cities"
-
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+    if models.storage_type == "db":
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        state_id = ""
+        name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-import os
 
 
-class State(BaseModel):
-    """State class"""
-    __tablename__ = 'states'
+class State(BaseModel, Base):
+    """ State class """
 
-    name = Column(String(128), nullable=False)
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    if models.storage_type == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="all, delete")
     else:
-        @property
-        def cities(self):
-            from models import storage
-            city_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
