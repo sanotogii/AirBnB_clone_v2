219e0a44b7bc51b146e904a5de8505558da35b21
TheOrbMaverick <enangita@gmail.com>
2024-04-24 23:26:53 +0100

    reprobe

--- models/state.py
+++ models/state.py
@@ -4,16 +4,14 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-import os
 import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    """Representation of state """
     if models.storage_type == 'db':
         __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
+        name = Column(String(128))
         cities = relationship("City", backref="state")
     else:
         name = ""
