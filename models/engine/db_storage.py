800044d4a29d9ccf892937dc03bb997c67ee9643
TheOrbMaverick <enangita@gmail.com>
2024-04-24 22:19:57 +0100

    reunite

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,28 +17,25 @@ classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 class DBStorage:
+    """interaacts with the MySQL database"""
+    
     __engine = None
     __session = None
 
     def __init__(self):
-        """interaacts with the MySQL database"""
-        __engine = None
-        __session = None
-
-        def __init__(self):
-            """Instantiate a DBStorage object"""
-            HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-            HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-            HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-            HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-            HBNB_ENV = getenv('HBNB_ENV')
-            self.__engine = __engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                        format(HBNB_MYSQL_USER,
-                                                HBNB_MYSQL_PWD,
-                                                HBNB_MYSQL_HOST,
-                                                HBNB_MYSQL_DB))
-            if HBNB_ENV == "test":
-                Base.metadata.drop_all(self.__engine)
+        """Instantiate a DBStorage object"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                    format(HBNB_MYSQL_USER,
+                                            HBNB_MYSQL_PWD,
+                                            HBNB_MYSQL_HOST,
+                                            HBNB_MYSQL_DB))
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """
