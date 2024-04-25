c7115a618754b091347eac3dcb0ae84bbfbdd39a
TheOrbMaverick <enangita@gmail.com>
2024-04-25 01:28:49 +0100

    reprobe

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     if models.storage_type == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
 
