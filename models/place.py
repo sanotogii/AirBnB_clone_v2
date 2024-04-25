e8f83822193af659c28736ec06d11a634bbed89b
TheOrbMaverick <enangita@gmail.com>
2024-04-25 01:19:45 +0100

    reprobe

--- models/place.py
+++ models/place.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, Integer, ForeignKey, Float
-import models
 
 
 class Place(BaseModel):
     """ A place to stay """
-
-    if models.storage_type == 'db':
-        """ Place class """
-        __tablename__ = 'places'
-
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        # Relationship with User
-        user = relationship("User", backref="places")
-        # Relationship with City
-        city = relationship("City", backref="places", cascade="all, delete")
-
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
