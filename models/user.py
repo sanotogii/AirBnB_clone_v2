8ebfca9e949fea472980c0af480e60ee74d8f08a
TheOrbMaverick <enangita@gmail.com>
2024-04-25 01:00:53 +0100

    reprobe

--- models/user.py
+++ models/user.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+import models
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """This class defines a user by various attributes"""
+
     if models.storage_type == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
