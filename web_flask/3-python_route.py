825bb14226b0540f1848fbb1d7bc911710b215ef
sanotogii <nasaereh@gmail.com>
2024-05-18 14:40:54 +0200

    task-4 +3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,6 +31,7 @@ def display(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text='is_cool'):
